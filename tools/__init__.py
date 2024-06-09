1478882ed63d316f5b63f5ab573fedb2b94bf84b
Marcelo Ecco <marceloecco1@gmail.com>
2024-06-09 20:01:27 -0300

    chore: delete forder

--- tools/__init__.py
+++
@@ -1,8 +0,0 @@
-from tools.annotator import Annotator
-from tools.cropper import Cropper
-from tools.drawer import Drawer
-from tools.label_handler import LabelHandler
-from tools.formatter import Formatter
-
-#TODO: split train test and validation
-#TODO: resize operations
